9dbe4c0e6d50ad898827e1e3a2477b2d5ebc7550
Zsombor Medgyesi <medgyesizs123@gmail.com>
2024-10-30 06:38:35 +0100

    Test Headless

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox', '--disable-gpu']
       }
     },
     restartOnFileChange: true
